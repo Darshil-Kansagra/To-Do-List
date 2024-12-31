312466f2a5d87f61b8806a37b61e23ecb7918a1a
Darshil <kansagradarshil@gmail.com>
2024-12-31 16:56:35 +0530

    new refersh update

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 var btn = document.getElementsByTagName("button");
 var itemjsonarray = [];
 update();
-
 btn[0].addEventListener("click", () => {
-  var tit = document.getElementById("title").value;
+  var title = document.getElementById("title").value;
   var des = document.getElementById("Description").value;
   if (localStorage.getItem("itemjson") == null) {
-    itemjsonarray.push([tit, des]);
+    itemjsonarray.push([title, des]);
     localStorage.setItem("itemjson", JSON.stringify(itemjsonarray));
   } else {
     itemjsonarraystr = localStorage.getItem("itemjson");
     itemjsonarray = JSON.parse(itemjsonarraystr);
-    itemjsonarray.push([tit, des]);
+    itemjsonarray.push([title, des]);
     localStorage.setItem("itemjson", JSON.stringify(itemjsonarray));
   }
+  document.getElementById("title").value = "";
+  document.getElementById("Description").value = "";
   update();
 });
 
